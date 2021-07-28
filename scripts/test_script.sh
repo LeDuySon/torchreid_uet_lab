98314e733f0bcb18d82d041323ea6776e15da790
Son <leduyson2632001@gmail.com>
2021-07-29 00:57:32 +0700

    update script training and testing

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,4 +1,4 @@
 config=$1
 models=$2
-root_folder=$3
-python scripts/test.py --config-file $config -s "uet_reid" -t "uet_reid" --root $root_folder test.evaluate True model.load_weights $models test.dist_metric cosine 
+#root_folder=$3
+python scripts/test.py --config-file $config -s "uet_reid" -t "uet_reid" --root "reid-data" test.evaluate True model.load_weights $models test.dist_metric cosine 
--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -1,5 +1,6 @@
 configs=$1
+models=$2
+python scripts/main.py --config-file $configs -s "uet_reid" -t "uet_reid" --transforms random_flip --root "reid-data" model.load_weights $models loss.name "triplet"
 
-python scripts/main.py --config-file $configs --transforms random_flip --root "reid-data"
 
 
