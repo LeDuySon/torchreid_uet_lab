4368566f24203eadb952c2e100fe5024cf65e4c2
Son <leduyson2632001@gmail.com>
2021-07-29 19:41:22 +0700

    update train script

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -1,6 +1,7 @@
 configs=$1
 models=$2
-python scripts/main.py --config-file $configs -s "uet_reid" -t "uet_reid" --transforms random_flip --root "reid-data" model.load_weights $models loss.name "triplet"
+save_dir=$3
+python scripts/main.py --config-file $configs -s "uet_reid" -t "uet_reid" --transforms random_flip --root "reid-data" model.load_weights $models loss.name "softmax" data.save_dir $save_dir
 
 
 
