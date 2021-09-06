4dd1ea40b6f91d756dd048b5afb9fd2817984a71
LeDuySon <leduyson2632001@gmail.com>
2021-09-06 20:52:07 +0700

    add args for combine 2 loss

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -1,7 +1,11 @@
 configs=$1
 models=$2
 save_dir=$3
-python scripts/main.py --config-file $configs -s "uet_reid" -t "uet_reid" --transforms random_flip --root "reid-data" model.load_weights $models loss.name "softmax" data.save_dir $save_dir
+epoch=15
+eval_freq=5
 
+python scripts/main.py --config-file $configs -s "uet_reid" -t "uet_reid" --transforms random_flip --root "reid-data" loss.name "triplet" sampler.train_sampler RandomIdentitySampler model.load_weights $models  data.save_dir $save_dir train.max_epoch $epoch test.eval_freq $eval_freq  
+
+#https://github.com/KaiyangZhou/deep-person-reid/issues/155#issuecomment-484363594_
 
 
